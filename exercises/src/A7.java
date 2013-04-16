c1ef8bf2248c0f2dcc6eb15b33d792ee705f174b
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-16 23:45:23 +0200

    implement knapsack

--- exercises/src/A7.java
+++ exercises/src/A7.java
@@ -1,4 +1,7 @@
+import java.awt.List;
 import java.io.PrintStream;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,8 +10,9 @@ class A7 {
 	private static class Knapsack {
 
 		private int n, W;
-		private int[] v, w, vs;
-		private int opt;
+		private int[] v, w;
+		ArrayList<Integer> vs;
+		private int[][] matrix;
 
 		public Knapsack(int n, int W, int[] v, int[] w) {
 			this.n = n;
@@ -19,15 +23,48 @@ class A7 {
 		}
 
 		private void solve() {
-			//TODO: implement this
+			matrix = new int[n + 1][];
+			for (int i = 0; i < matrix.length; i++) {
+				matrix[i] = new int[W + 1];
+			}
+
+			for (int i = 0; i < W; i++) {
+				matrix[0][i] = 0;
+			}
+
+			for (int i = 1; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[i].length; j++) {
+					if (j >= w[i - 1]) {
+						matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i - 1][j - w[i - 1]] + v[i - 1]);
+					} else {
+						matrix[i][j] = matrix[i - 1][j];
+					}
+				}
+			}
+
+			vs = new ArrayList<Integer>();
+			int cn = n;
+			int cw = W;
+			while (cn > 0) {
+				if (w[cn - 1] <= cw && matrix[cn][cw] == matrix[cn - 1][cw - w[cn - 1]] + v[cn - 1]) {
+					vs.add(cn);
+					cw = cw - w[cn - 1];
+				}
+				cn = cn - 1;
+			}
 		}
 
 		public int getOpt() {
-			return opt;
+			return matrix[n][W];
 		}
 
 		public int[] getVs() {
-			return vs;
+			int[] arr = new int[vs.size()];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = vs.get(i);
+			}
+			Arrays.sort(arr);
+			return arr;
 		}
 
 	}
