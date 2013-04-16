0a81fa722fdda482fe51474cf0b611b810901885
Lukas Elmer <lukas.elmer@gmail.com>
2013-04-16 22:15:49 +0200

    scaffold

--- exercises/src/A7.java
+++ exercises/src/A7.java
@@ -4,24 +4,60 @@ import java.util.Scanner;
 
 class A7 {
 
+	private static class Knapsack {
+
+		private int n, W;
+		private int[] v, w, vs;
+		private int opt;
+
+		public Knapsack(int n, int W, int[] v, int[] w) {
+			this.n = n;
+			this.W = W;
+			this.v = v;
+			this.w = w;
+			solve();
+		}
+
+		private void solve() {
+			//TODO: implement this
+		}
+
+		public int getOpt() {
+			return opt;
+		}
+
+		public int[] getVs() {
+			return vs;
+		}
+
+	}
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		try {
-			int lines = sc.nextInt();
+			int t = sc.nextInt();
 
-			for (int j = 0; j < lines; j++) {
+			for (int j = 0; j < t; j++) {
 				int n = sc.nextInt();
+				int W = sc.nextInt();
 
-				int[] array = new int[n];
+				int[] v = new int[n];
+				int[] w = new int[n];
 
 				for (int i = 0; i < n; i++) {
-					int x = sc.nextInt();
-					array[i] = x;
+					v[i] = sc.nextInt();
+				}
+				for (int i = 0; i < n; i++) {
+					w[i] = sc.nextInt();
 				}
 
-				int m = select(array, 0, array.length - 1, medianPos(array.length), System.out);
-				System.out.println(array[m]);
+				Knapsack k = new Knapsack(n, W, v, w);
+				System.out.print(k.getOpt());
+				for (int i : k.getVs()) {
+					System.out.print(" " + i);
+				}
+				System.out.println();
 			}
 
 		} finally {
@@ -29,83 +65,4 @@ class A7 {
 		}
 	}
 
-	public static int select(int[] array, int l, int r, int i, PrintStream out) {
-		int v = selectPivot(array, l, r, out);
-
-		if (out != null)
-			out.print(array[v] + " ");
-
-		if (l >= r)
-			return l;
-
-		int m = split(array, l, r, v);
-
-		if (i == m - l)
-			return m;
-		else if (i > m - l)
-			return select(array, m + 1, r, i - (m + 1 - l), null);
-		else
-			return select(array, l, m - 1, i, null);
-	}
-
-	public static int selectPivot(int[] a, int l, int r, PrintStream out) {
-		int[] medians = medians(a, l, r);
-
-		if (out != null)
-			for (int i = 0; i < medians.length; i++)
-				out.print(medians[i] + " ");
-
-		if (medians.length <= 5)
-			return index(a, l, r, median(medians));
-
-		int pivot = medians[select(medians, 0, medians.length - 1, medianPos(medians.length), null)];
-		return index(a, l, r, pivot);
-	}
-
-	public static void swap(int[] array, int i, int j) {
-		int tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-	}
-
-	public static int medianPos(int length) {
-		return ((length + 1) / 2) - 1;
-	}
-
-	public static int median(int[] array) {
-		Arrays.sort(array);
-		return array[medianPos(array.length)];
-	}
-
-	public static int index(int[] array, int l, int r, int v) {
-		for (int i = l; i <= r; i++)
-			if (array[i] == v)
-				return i;
-
-		return -1;
-	}
-
-	public static int split(int[] array, int l, int r, int v) {
-		int p = array[v];
-		int s = l;
-
-		swap(array, v, r - 1);
-		for (int i = l; i <= r; i++) {
-			if (array[i] < p) {
-				swap(array, i, s);
-				s++;
-			}
-		}
-
-		swap(array, s, r - 1);
-		return s;
-	}
-
-	public static int[] medians(int[] a, int l, int r) {
-		int[] medians = new int[(int) Math.ceil((r - l + 1) / 5.0)];
-		for (int i = l; i <= r; i = i + 5) {
-			medians[(i - l) / 5] = median(Arrays.copyOfRange(a, i, Math.min(i + 5, r + 1)));
-		}
-		return medians;
-	}
 }
